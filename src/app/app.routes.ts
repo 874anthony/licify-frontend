01367c0dd0d99d001fff41603d28f71eef15da58
Anthony Acosta M <anthony082008@hotmail.es>
2024-01-07 19:17:39 -0500

    Added login and signup pages and shadows to boxes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,8 +2,18 @@ import { Routes } from '@angular/router';
 import { PagesComponent } from './pages/pages.component';
 import { AllProjectsComponent } from './pages/all-projects/all-projects.component';
 import { ProjectComponent } from './pages/project/project.component';
+import { LoginComponent } from './auth/login/login.component';
+import { SignupComponent } from './auth/signup/signup.component';
 
 export const routes: Routes = [
+  {
+    path: 'login',
+    component: LoginComponent,
+  },
+  {
+    path: 'signup',
+    component: SignupComponent,
+  },
   {
     path: 'dashboard',
     component: PagesComponent,
